a3e7448dbe39f531f6df047a98a273daa5489192
赵悦茗 <164242417+marco-zym@users.noreply.github.com>
2024-11-28 14:47:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function zh() {
+    window.open("https://marco-zym.github.io/strong-password/zh", "_self")
+}
+
+function en() {
+    window.open("https://marco-zym.github.io/strong-password/en", "_self")
+}
